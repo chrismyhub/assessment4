d5eeeb6e519d088ad78f608ed17bf24200861c95
chrismyhub <keyoch@gmail.com>
2023-08-13 04:04:41 +0000

    Added code to web_scrape.sh to remove "</div>" and also to remove everything in between "<h1 " and "</h1>".

--- DRAFT/web_scrape.sh
+++ DRAFT/web_scrape.sh
@@ -33,8 +33,11 @@ function dump_webpage() {
 #This will remove all "&amp;" and replace it with "and": 's/&amp;/and/g'
 #This will remove all "&#8217;" and replace it with "'": "s/&#8217;/'/g"
 #This will remove all "</ul>": "s/<\/ul>//g"
+#This will remove everything in between "<h1 " and "</h1>": '/^<h1 /,/^\<\/h1>/{/^<h1 /!{/^\<\/h1>/!d}}'
+#This will remove all "</div>": "s/<\/div>//g"
+
 function strip_html() {
-    grep -A 20 "<h3>.*8211" $DATA | sed -e 's/<h3>//g' -e 's/<\/h3>//g' -e 's/&#8211//g' -e 's/^[ \t]*//' -e 's/<strong>//g' -e 's/<\/strong>//g' -e 's/&amp;/and/g' -e "s/&#8217;/'/g" -e "s/<\/ul>//g" > temp.txt && cp temp.txt $DATA && rm temp.txt
+    grep -A 20 "<h3>.*8211" $DATA | sed -e 's/<h3>//g' -e 's/<\/h3>//g' -e 's/&#8211//g' -e 's/^[ \t]*//' -e 's/<strong>//g' -e 's/<\/strong>//g' -e 's/&amp;/and/g' -e "s/&#8217;/'/g" -e "s/<\/ul>//g" -e "s/<ul>//g" -e '/^<h1 /,/^\<\/h1>/{/^<h1 /!{/^\<\/h1>/!d}}' -e "s/<\/div>//g" > temp.txt && cp temp.txt $DATA && rm temp.txt
 
 }
 
